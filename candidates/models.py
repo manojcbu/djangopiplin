921055abd6f8cab94b65c96ea47856ddf7c19050
Rajat Dev Sharma <rajatdevsharma@gmail.com>
2022-05-30 01:55:15 +0530

    Update Skills to ManyToMany relationship

--- candidates/models.py
+++ candidates/models.py
@@ -4,40 +4,28 @@ from django.db.models.functions import Length
 
 models.TextField.register_lookup(Length)
 
-# Choices for Gender Input
-GENDER = (
-    ("Male", "Male"),
-    ("Female", "Female"),
-    ("NA", "Prefer Not To Say")
-)
 
-RESUME_STATUS = (
-    ("PENDING", "In Queue"),
-    ("SUCCESS", "Success"),
-    ("FAILED", "Failed")
-)
+class Skill(models.Model):
+    skill = models.TextField(max_length=255, unique=True)
 
-SPECIALIZATION = (
-    ("Artificial Intelligence", "Artificial Intelligence"),
-    ("Computer-Human Interface", "Computer-Human Interface"),
-    ("Game Design", "Game Design"),
-    ("Networks and Security", "Networks and Security"),
-    ("Computer Graphics", "Computer Graphics"),
-    ("Data Science", "Data Science"),
-    ("Others", "Others")
-)
-
-INSTITUTE = (
-    ("Institute 1", "Institute 1"),
-    ("Institute 2", "Institute 2"),
-    ("Institute 3", "Institute 3"),
-    ("Institute 4", "Institute 4"),
-    ("Institute 5", "Institute 5"),
-    ("Institute 6", "Institute 6")
-)
+    def __str__(self):
+        return self.skill
 
 
 class Candidate(models.Model):
+    # Choices for Gender Input
+    GENDER = (
+        ("Male", "Male"),
+        ("Female", "Female"),
+        ("NA", "Prefer Not To Say")
+    )
+    # Choices for Resume Status
+    RESUME_STATUS = (
+        ("PENDING", "In Queue"),
+        ("SUCCESS", "Success"),
+        ("FAILED", "Failed")
+    )
+
     full_name = models.TextField(max_length=255)
     aadhaar_id = models.TextField(max_length=16)
     dob = models.DateField()
@@ -53,6 +41,7 @@ class Candidate(models.Model):
     experience = models.IntegerField()
     resume_status = models.CharField(max_length=10, choices=RESUME_STATUS, default='PENDING')
     resume_path = models.TextField(null=True)
+    skills = models.ManyToManyField(Skill)
 
     # TODO: Add missing check constraints
     class Meta:
@@ -68,6 +57,16 @@ class Candidate(models.Model):
 
 
 class Specialization(models.Model):
+    SPECIALIZATION = (
+        ("Artificial Intelligence", "Artificial Intelligence"),
+        ("Computer-Human Interface", "Computer-Human Interface"),
+        ("Game Design", "Game Design"),
+        ("Networks and Security", "Networks and Security"),
+        ("Computer Graphics", "Computer Graphics"),
+        ("Data Science", "Data Science"),
+        ("Others", "Others")
+    )
+
     specialization = models.TextField(max_length=255, choices=SPECIALIZATION, unique=True)
 
     def __str__(self):
@@ -75,17 +74,19 @@ class Specialization(models.Model):
 
 
 class Institute(models.Model):
+    INSTITUTE = (
+        ("Institute 1", "Institute 1"),
+        ("Institute 2", "Institute 2"),
+        ("Institute 3", "Institute 3"),
+        ("Institute 4", "Institute 4"),
+        ("Institute 5", "Institute 5"),
+        ("Institute 6", "Institute 6")
+    )
+
     institute = models.TextField(max_length=255, choices=INSTITUTE, unique=True)
 
     def __str__(self):
         return self.institute
-
-
-class Skill(models.Model):
-    skill = models.TextField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.skill
 
 
 class Education(models.Model):
@@ -97,6 +98,6 @@ class Education(models.Model):
     year_of_passing = models.DateField()
 
 
-class CandidateSkills(models.Model):
-    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+# class CandidateSkills(models.Model):
+#     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+#     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
